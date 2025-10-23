8a53ccfb86a92381d773d6efd465c498fde11ef6
redslime <hallopiu@gmail.com>
2025-10-23 16:41:28 +0200

    Test new seo meta

--- pages/play.vue
+++ pages/play.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-definePageMeta({
-  ssr: true
-})
-
 const query = useRoute().query
 console.log(query)
 
-if(query && query.r) {
+if(query.r) {
+  // useSeoMeta({
+  //   title: 'hardstyle.gg',
+  //   ogTitle: 'hardstyle.gg',
+  //   description: 'Daily hardstyle challenge to compete with friends.',
+  //   ogDescription: 'Daily hardstyle challenge to compete with friends.',
+  //   ogImage: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
+  // })
+
   useSeoMeta({
     title: 'hardstyle.gg',
     ogTitle: 'hardstyle.gg',
     description: 'Daily hardstyle challenge to compete with friends.',
     ogDescription: 'Daily hardstyle challenge to compete with friends.',
-    ogImage: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
+    ogUrl: 'https://hardstylegg.redslime.xyz/',
+    ogSiteName: 'hardstyle.gg',
+    ogLocale: 'en_US',
+    ogType: 'website',
+    twitterSite: 'hardstyle.gg',
+    twitterDescription: 'Daily hardstyle challenge to compete with friends.',
+    twitterImage: {
+      url: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
+      alt: 'hardstyle.gg',
+      width: 550,
+      height: 140,
+    },
+    twitterCard: 'summary_large_image',
+    themeColor: '#3ABDF8',
   })
 }
 
