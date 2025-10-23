d67fb6ce364013ae6ac60fd80903e32ddb65f369
redslime <hallopiu@gmail.com>
2025-10-23 16:55:09 +0200

    Test other variations

--- pages/play.vue
+++ pages/play.vue
@@ -3,35 +3,6 @@ const query = useRoute().query
 console.log(query)
 
 if(query.r) {
-  // useSeoMeta({
-  //   title: 'hardstyle.gg',
-  //   ogTitle: 'hardstyle.gg',
-  //   description: 'Daily hardstyle challenge to compete with friends.',
-  //   ogDescription: 'Daily hardstyle challenge to compete with friends.',
-  //   ogImage: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
-  // })
-
-  // useSeoMeta({
-  //   title: 'hardstyle.gg',
-  //   ogTitle: 'hardstyle.gg',
-  //   description: 'Daily hardstyle challenge to compete with friends.',
-  //   ogDescription: 'Daily hardstyle challenge to compete with friends.',
-  //   ogUrl: 'https://hardstylegg.redslime.xyz/',
-  //   ogSiteName: 'hardstyle.gg',
-  //   ogLocale: 'en_US',
-  //   ogType: 'website',
-  //   twitterSite: 'hardstyle.gg',
-  //   twitterDescription: 'Daily hardstyle challenge to compete with friends.',
-  //   twitterImage: {
-  //     url: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
-  //     alt: 'hardstyle.gg',
-  //     width: 550,
-  //     height: 140,
-  //   },
-  //   twitterCard: 'summary_large_image',
-  //   themeColor: '#3ABDF8',
-  // })
-
   useHead({
     meta: [
       {name: 'og:title', content: 'hardstyle.gg'},
@@ -46,11 +17,38 @@ if(query.r) {
       {name: 'theme-color', content: '#3ABDF8'},
     ]
   })
+}
 
+if(query.f) {
+  useHead({
+    meta: [
+      {name: 'twitter:image:src', content: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.f},
+      {name: 'twitter:image:alt', content: 'hardstyle.gg'},
+      {name: 'twitter:image:width', content: '550'},
+      {name: 'twitter:image:height', content: '140'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'theme-color', content: '#3ABDF8'},
+    ]
+  })
+}
+
+if(query.g) {
+  useHead({
+    meta: [
+      {name: 'og:description', content: 'Daily hardstyle challenge to compete with friends.'},
+      {name: 'twitter:description', content: 'Daily hardstyle challenge to compete with friends.'},
+      {name: 'twitter:image:src', content: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.g},
+      {name: 'twitter:image:alt', content: 'hardstyle.gg'},
+      {name: 'twitter:image:width', content: '550'},
+      {name: 'twitter:image:height', content: '140'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'theme-color', content: '#3ABDF8'},
+    ]
+  })
 }
 
 
-// navigateTo('/')
+navigateTo('/')
 </script>
 
 <template>
