47f32952affb32209a05cc320fe21aa7a574fb94
redslime <hallopiu@gmail.com>
2025-10-23 15:55:56 +0200

    Test open graph under useHead()

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,32 @@ import type {GameContainer, PackedDayData} from "~/types/models";
 
 const query = useRoute().query
 
+// if(query.r) {
+//   definePageMeta({
+//     title: 'hardstyle.gg',
+//     ogTitle: 'hardstyle.gg',
+//     description: 'Daily hardstyle challenge to compete with friends.',
+//     ogDescription: 'Daily hardstyle challenge to compete with friends.',
+//     ogImage: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
+//   })
+// }
+
 if(query.r) {
-  useSeoMeta({
+  useHead({
     title: 'hardstyle.gg',
-    ogTitle: 'hardstyle.gg',
-    description: 'Daily hardstyle challenge to compete with friends.',
-    ogDescription: 'Daily hardstyle challenge to compete with friends.',
-    ogImage: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r,
+    meta: [
+      { name: 'description', content: 'Daily hardstyle challenge to compete with friends.' },
+      { name: 'og:title', content: 'hardstyle.gg' },
+      { name: 'og:description', content: 'Daily hardstyle challenge to compete with friends.' },
+      { name: 'og:image', content: 'https://hardstylegg.redslime.xyz/api/og?r=' + query.r },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   })
 }
+
+useRouter().replace({ query: {} })
 
 const { data, pending, error, refresh } = await useAsyncData(
     () => $fetch('/api/today')
