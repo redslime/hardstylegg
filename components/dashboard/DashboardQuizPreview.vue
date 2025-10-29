0dd55c9325036239a2726bbb3a8a7d6effdbf829
redslime <hallopiu@gmail.com>
2025-10-29 16:50:34 +0100

    Make generic DashboardGamePreview

--- components/dashboard/DashboardQuizPreview.vue
+++ components/dashboard/DashboardQuizPreview.vue
@@ -1,37 +1,21 @@
 <script setup lang="ts">
 import type {QuizContainer} from "~/types/gameModels";
-import {getDashboardData, getScheduleForGame} from "~/utils/dashboard";
-import type {ScheduleDay} from "~/types/models";
 
-const emit = defineEmits(['clicked'])
 const { quiz } = defineProps({
   quiz: { type: Object as PropType<QuizContainer>, required: true }
 })
-const dashboardData = await getDashboardData()
-const scheduleData: ScheduleDay | undefined = getScheduleForGame(7, quiz.id!!)
-const editor = dashboardData.editors.find(e => e.id === quiz.created_by)
 </script>
 
 <template>
-  <div class="bg-base-200 p-3 max-w-[600px] rounded-lg cursor-pointer" @click="emit('clicked')">
-    <div class="text-2xl font-bold">{{ quiz.title }}</div>
-    <div class="flex flex-wrap gap-1 mb-4">
-      <div class="badge badge-success badge-soft badge-xs font-mono" v-if="scheduleData">Scheduled: {{ scheduleData.dayFriendly }}</div>
-      <div class="badge badge-neutral badge-xs font-mono">ID: {{ quiz.id }}</div>
-      <div class="badge badge-neutral badge-xs font-mono">
-        Created by {{ editor?.name }}
-      </div>
-    </div>
-    <div class="flex flex-wrap gap-2">
-      <div v-for="item in quiz.items" :key="item.id" class="badge badge-outline"
-           :class="{
+  <DashboardGamePreview :type-id="7" :container="quiz" :title="quiz.title">
+    <div v-for="item in quiz.items" :key="item.id" class="badge badge-outline"
+         :class="{
               'badge-success': item.correct,
               'badge-error': !item.correct
             }">
-        {{ item.text }}
-      </div>
+      {{ item.text }}
     </div>
-  </div>
+  </DashboardGamePreview>
 </template>
 
 <style scoped>
--- components/dashboard/DashboardTimelinePreview.vue
+++ components/dashboard/DashboardTimelinePreview.vue
@@ -1,31 +1,16 @@
 <script setup lang="ts">
 import type {TimelineContainer} from "~/types/gameModels";
-import {getDashboardData, getScheduleForGame} from "~/utils/dashboard";
-import type {ScheduleDay} from "~/types/models";
 
-const emit = defineEmits(['clicked'])
 const { timeline } = defineProps({
   timeline: { type: Object as PropType<TimelineContainer>, required: true }
 })
-const dashboardData = await getDashboardData()
-const scheduleData: ScheduleDay | undefined = getScheduleForGame(8, timeline.id!!)
-const editor = dashboardData.editors.find(e => e.id === timeline.created_by)
+const goal = timeline.goal
 </script>
 
 <template>
-  <div class="bg-base-200 p-3 max-w-[600px] rounded-lg cursor-pointer" @click="emit('clicked')">
-    <div class="text-2xl font-bold">{{ timeline.title }}</div>
-    <div class="flex flex-wrap gap-1 mb-4">
-      <div class="badge badge-success badge-soft badge-xs font-mono" v-if="scheduleData">Scheduled: {{ scheduleData.dayFriendly }}</div>
-      <div class="badge badge-neutral badge-xs font-mono">ID: {{ timeline.id }}</div>
-      <div class="badge badge-neutral badge-xs font-mono">
-        Created by {{ editor?.name }}
-      </div>
-    </div>
-    <div class="flex flex-wrap gap-2">
-      <div class="badge badge-outline badge-success">{{ timeline.goal }}</div>
-    </div>
-  </div>
+  <DashboardGamePreview :type-id="8" :container="timeline" :title="timeline.title">
+    <div class="badge badge-outline badge-success">{{ goal }}</div>
+  </DashboardGamePreview>
 </template>
 
 <style scoped>
