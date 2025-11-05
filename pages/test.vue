fe30dc4edef7e96a2526445cd1d4a7dfb9bc07b1
redslime <hallopiu@gmail.com>
2025-11-05 10:39:34 +0100

    Test

---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {resolve} from "pathe";
+
+const path = resolve('../public/artwork')
+const path1 = resolve('public/artwork')
+</script>
+
+<template>
+  {{ path }}
+  <br>
+  {{ path1 }}
+  <br>
+  {{ resolve('../public/artwork') }}
+</template>
+
+<style scoped>
+
+</style>
