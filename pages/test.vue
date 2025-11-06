90179665c918fa2a768b3d03b9c1b696a7b562e5
redslime <hallopiu@gmail.com>
2025-11-06 10:46:19 +0100

    Test ios audio unlocking on silent mode

---
+++ pages/test.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+import {Howl} from "howler";
+
+const playing = ref(false)
+let howl: Howl | null = null
+
+onMounted(() => {
+  howl = new Howl({
+    src: [`/heardle/7b827509-33c4-4083-bb3c-7a36d751d76d.mp3`],
+    preload: true,
+    volume: 0.2
+  });
+})
+
+function unlockIOSAudio() {
+  const silent = document.getElementById("silent-audio") as HTMLAudioElement
+  silent.volume = 0
+  silent.play().catch(() => {})
+}
+
+function play() {
+  unlockIOSAudio()
+  howl?.play()
+  playing.value = true
+}
+</script>
+
+<template>
+  <audio id="silent-audio" playsinline preload="auto"></audio>
+  <button class="btn btn-primary btn-outline" @click="play()">play very cool song</button>
+  <p v-if="playing">ya hear it?</p>
+</template>
+
+<style scoped>
+
+</style>
