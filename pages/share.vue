3933e0c9be6056988002635cf6ad5c078e7496c3
redslime <hallopiu@gmail.com>
2025-11-07 10:38:44 +0100

    Actually serve the new embed on /share (pepega)

--- pages/share.vue
+++ pages/share.vue
@@ -14,6 +14,19 @@ if(query.r) {
       {name: 'theme-color', content: '#3ABDF8'},
     ]
   })
+} else if(query.c) {
+  useHead({
+    meta: [
+      {name: 'og:description', content: 'Daily hardstyle challenge to compete with friends.'},
+      {name: 'twitter:description', content: 'Daily hardstyle challenge to compete with friends.'},
+      {name: 'twitter:image:src', content: 'https://hardstyle.gg/api/og?c=' + query.c},
+      {name: 'twitter:image:alt', content: 'hardstyle.gg'},
+      {name: 'twitter:image:width', content: '550'},
+      {name: 'twitter:image:height', content: '140'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'theme-color', content: '#3ABDF8'},
+    ]
+  })
 }
 
 onMounted(() => {
