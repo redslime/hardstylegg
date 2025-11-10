1274d6afb3ad10c5391cb15c2bb70005a8247cdf
redslime <hallopiu@gmail.com>
2025-11-10 14:15:13 +0100

    Finalize embed endpoint

--- pages/share.vue
+++ pages/share.vue
@@ -1,44 +1,23 @@
 <script setup lang="ts">
 const query = useRoute().query
+let imageUrl = "https://hardstyle.gg/api/og"
 
-if(query.r) {
-  useHead({
-    meta: [
-      {name: 'og:description', content: 'Daily hardstyle challenge to compete with friends.'},
-      {name: 'twitter:description', content: 'Daily hardstyle challenge to compete with friends.'},
-      {name: 'twitter:image:src', content: 'https://hardstyle.gg/api/og?r=' + query.r},
-      {name: 'twitter:image:alt', content: 'hardstyle.gg'},
-      {name: 'twitter:image:width', content: '550'},
-      {name: 'twitter:image:height', content: '140'},
-      {name: 'twitter:card', content: 'summary_large_image'},
-      {name: 'theme-color', content: '#3ABDF8'},
-    ]
-  })
-} else if(query.c) {
-  // useHead({
-  //   meta: [
-  //     {name: 'og:description', content: 'Daily hardstyle challenge to compete with friends.'},
-  //     {name: 'twitter:description', content: 'Daily hardstyle challenge to compete with friends.'},
-  //     {name: 'twitter:image:src', content: 'https://hardstyle.gg/api/og?c=' + query.c},
-  //     {name: 'twitter:image:alt', content: 'hardstyle.gg'},
-  //     {name: 'twitter:image:width', content: '550'},
-  //     {name: 'twitter:image:height', content: '140'},
-  //     {name: 'twitter:card', content: 'summary_large_image'},
-  //     {name: 'theme-color', content: '#3ABDF8'},
-  //   ]
-  // })
-
-  useHead({
-    meta: [
-      { property: 'og:description', content: 'Daily hardstyle challenge to compete with friends.' },
-      { property: 'og:image', content: 'https://hardstyle.gg/api/og?c=' + query.c },
-      { property: 'og:image:alt', content: 'hardstyle.gg' },
-      { property: 'og:image:width', content: '550' },
-      { property: 'og:image:height', content: '140' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-    ]
-  })
+if(query.c) {
+  imageUrl += "?c=" + query.c
+} else if(query.r) {
+  imageUrl += "?r=" + query.r
 }
+
+useHead({
+  meta: [
+    { property: 'og:description', content: 'Daily hardstyle challenge to compete with friends.' },
+    { property: 'og:image', content: imageUrl },
+    { property: 'og:image:alt', content: 'hardstyle.gg' },
+    { property: 'og:image:width', content: '550' },
+    { property: 'og:image:height', content: '140' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+  ]
+})
 
 onMounted(() => {
   navigateTo('/')
