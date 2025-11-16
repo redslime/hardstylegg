0e75cd19cfdd6c7f03fd91f824882d8454b3f053
redslime <hallopiu@gmail.com>
2025-11-16 14:47:16 +0100

    Update upcoming/past badge on preview items

--- components/dashboard/DashboardGamePreviewHeader.vue
+++ components/dashboard/DashboardGamePreviewHeader.vue
@@ -13,6 +13,9 @@ const { typeId, container, title, pointer } = defineProps({
 const { user } = useUserSession()
 const dashboardData = await getDashboardData()
 const scheduleData = computed<ScheduleDay | undefined>(() => getScheduleForGame(typeId, container.id))
+const todayId = computed(() => dashboardData.schedule.todayId)
+const upcoming = computed(() => todayId.value && scheduleData.value && todayId.value <= scheduleData.value.day)
+const past = computed(() => todayId.value && scheduleData.value && todayId.value > scheduleData.value.day)
 const example = computed(() => container.id === 1)
 const editable = computed(() => user.value.admin || !scheduleData || !scheduleData.value || example.value)
 const editor = computed(() => dashboardData.editors.find(e => e.id === container.created_by))
@@ -28,7 +31,8 @@ function click() {
   <div class="bg-base-200 p-3 max-w-[600px] rounded-lg" :class="{'cursor-pointer': pointer && editable}" @click="click()">
     <div class="text-2xl font-bold">{{ title }}</div>
     <div class="flex flex-wrap gap-1 mb-4">
-      <div class="badge badge-success badge-soft badge-xs font-mono" v-if="scheduleData">Scheduled: {{ scheduleData.dayFriendly }}</div>
+      <div class="badge badge-warning badge-soft badge-xs font-mono" v-if="past">Played on: {{ scheduleData!!.dayFriendly }}</div>
+      <div class="badge badge-success badge-soft badge-xs font-mono" v-if="upcoming">Scheduled: {{ scheduleData!!.dayFriendly }}</div>
       <div class="badge badge-accent badge-soft badge-xs font-mono" v-if="example">Example</div>
       <div class="badge badge-neutral badge-xs font-mono">ID: {{ container.id }}</div>
       <div class="badge badge-neutral badge-xs font-mono">
