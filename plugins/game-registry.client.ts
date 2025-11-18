2f48f2c46586d72fd71693bf97b3397d18faf8a3
redslime <hallopiu@gmail.com>
2025-11-19 00:15:07 +0100

    Make sure registerGames() truly only runs on the client

--- plugins/game-registry.client.ts
+++ plugins/game-registry.client.ts
@@ -1,5 +1,7 @@
 import {registerGames} from "~/utils/game/clientGameRegistry";
 
 export default defineNuxtPlugin(() => {
-    registerGames()
+    if(import.meta.client) {
+        registerGames()
+    }
 });
