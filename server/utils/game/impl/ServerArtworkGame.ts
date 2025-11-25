c2f4ca917a748c9931ceab46e581b12549be06d9
redslime <hallopiu@gmail.com>
2025-11-26 00:43:43 +0100

    Update Artwork icon

--- server/utils/game/impl/ServerArtworkGame.ts
+++ server/utils/game/impl/ServerArtworkGame.ts
@@ -83,7 +83,7 @@ export class ServerArtworkGame extends ServerGameDef<ArtworkContainer> {
     }
 
     override getPreviewIcon(): string {
-        return '<path stroke="currentColor" fill="none" stroke-width="1.5" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />';
+        return '<path stroke="currentColor" fill="none" stroke-width="1.5" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />';
     }
 
     override async getPreviewDetails(reportItem: ReportItem): Promise<string> {
