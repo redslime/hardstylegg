f12c74845e53b57fac2371e35b7dbd6ed2d25379
redslime <hallopiu@gmail.com>
2025-11-26 00:08:47 +0100

    Fix Timetable og/preview icon

--- server/utils/game/impl/ServerTimetableGame.ts
+++ server/utils/game/impl/ServerTimetableGame.ts
@@ -115,7 +115,7 @@ export class ServerTimetableGame extends ServerGameDef<TimetableContainer> {
     }
 
     override getPreviewIcon(): string {
-        return '<path stroke="currentColor" fill="none" stroke-width="1.5" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />'
+        return '<path stroke="currentColor" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />'
     }
 
     override async getPreviewDetails(reportItem: ReportItem): Promise<string> {
