bb77aae712113da4dcf17020d3cc8062a9e2c824
redslime <hallopiu@gmail.com>
2026-03-19 14:32:10 +0100

    Stylize wordle summary

--- components/games/wordle/WordleSummary.vue
+++ components/games/wordle/WordleSummary.vue
@@ -6,10 +6,15 @@ const { container, reports } = defineProps({
   container: { type: Object as PropType<WordleContainer>, required: true },
   reports: { type: Array as PropType<GameReportFlat[]>, required: true }
 })
+const letters = container.artist.name.split("")
 </script>
 
 <template>
-  <div class="badge badge-outline badge-info">{{ container.artist.name }}</div>
+  <div class="flex gap-1">
+    <div v-for="letter in letters" class="w-8 h-8 text-xl bg-[#6aaa64] rounded-sm text-center text-white uppercase font-bold">
+      {{ letter }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
