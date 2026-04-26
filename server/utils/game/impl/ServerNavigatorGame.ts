ca6add03532079ab1a2ccdc7ff9b3235c6ceb6a7
redslime <hallopiu@gmail.com>
2026-04-27 00:36:59 +0200

    Adjust navigator icon

--- server/utils/game/impl/ServerNavigatorGame.ts
+++ server/utils/game/impl/ServerNavigatorGame.ts
@@ -85,7 +85,8 @@ export class ServerNavigatorGame extends ServerGameDef<NavigatorContainer> {
     }
 
     override getPreviewIcon(): string {
-        return '<path fill="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m0 12c0-6.627 5.373-12 12-12s12 5.373 12 12-5.373 12-12 12c-6.627 0-12-5.373-12-12zm2.017 0c0 5.513 4.469 9.983 9.983 9.983s9.983-4.469 9.983-9.983c0-5.513-4.469-9.983-9.983-9.983-5.513 0-9.983 4.469-9.983 9.983zm8.278-.928c0-.001 0-.002 0-.002 0-.005.002-.01.006-.013.066-.108.136-.202.213-.289l-.002.002c.02-.024.041-.046.062-.068.071-.076.147-.146.228-.209l.004-.003c.027-.021.056-.037.083-.057s.075-.054.115-.078l6.483-3.795-3.783 6.464v.002c0 .005-.003.01-.007.012-.066.109-.136.203-.213.29l.002-.002c-.02.024-.041.046-.061.068-.071.076-.148.145-.229.208l-.004.003c-.027.021-.055.038-.083.057s-.075.054-.115.078l-6.483 3.796z" />';
+        return '<path fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M21,12c0,4.97-4.03,9-9,9S3,16.97,3,12,7.03,3,12,3s9,4.03,9,9Z"/>' +
+            '<path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M11.02,11.26l-2.44,4.16,4.16-2.44c.1-.06.18-.14.24-.24l2.44-4.16-4.16,2.44c-.1.06-.18.14-.24.24Z"/>'
     }
 
     override async getPreviewDetails(reportItem: ReportItem): Promise<string> {
