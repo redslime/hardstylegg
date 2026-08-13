3aff067ea0d2354296b7b7ddc25afbc6c5a7aded
redslime <hallopiu@gmail.com>
2026-08-13 14:30:07 +0200

    Fix improper unmatched path handling

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({ // avoid <Header> rendering on ssr
+  layout: 'plain'
+})
+throw createError({
+  statusCode: 404,
+  statusMessage: 'Page not found',
+})
+</script>
+
+<template>
+  <div />
+</template>
